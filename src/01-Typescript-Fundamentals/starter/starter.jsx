2ddc2ddaaca6f97b589a504b48128e030cdfe492
Shakim Harris <amenraprod@gmail.com>
2022-01-10 15:41:15 -0500

    added one file

---
+++ src/01-Typescript-Fundamentals/starter/starter.jsx
@@ -0,0 +1,15 @@
+/*** Here we have some basic Javascript code and we need to highlight the difference between Javascript
+ * syntax and Typescript syntax, so this file comes already built with some basic Javascript code. 
+ * In this lesson, we will convert the Javascript into Typescript
+ */
+
+
+import React from 'react';
+
+export default function LogsJavascriptData () {
+    let str = "Shakim";
+    console.log(str);
+    return str;
+    
+}
+
