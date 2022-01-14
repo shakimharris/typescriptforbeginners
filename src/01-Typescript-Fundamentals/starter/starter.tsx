7338b651a1d4addef0afa49193831e4db2cf6864
Shakim Harris <amenraprod@gmail.com>
2022-01-14 00:39:11 -0500

    lesson one

--- src/01-Typescript-Fundamentals/starter/starter.tsx
+++ src/01-Typescript-Fundamentals/starter/starter.tsx
@@ -4,12 +4,35 @@
  */
 
 
-import React from 'react';
+import React, { FunctionComponent }from 'react';
+import { StructuredType } from 'typescript';
 
-export default function LogsJavascriptData () {
-    let str = "Shakim";
-    console.log(str);
-    return str;
-    
+const BasicTypescriptTypes: FunctionComponent = () => {
+    let today: string = "01/13/2022";
+    let tomorrow: string = "01/14/2022";
+    let isLoggedIn: boolean = false;
+    let numberOfPresidents: number = 45;
+    let isEmptyValue: null = null;
+    let RegEx: RegExp = /foo/;
+
+    const usernames: Array<number> = [1, 2, 3, 4, 5];
+
+    interface users {
+        username: string;
+        email: string;
+    }
+
+    const listofusers: users = {
+        username: "Shakim",
+        email: "shakim@yahoo.com"
+    }
+
+    return (
+        <>
+            {usernames}
+            {listofusers}
+        </>
+    )
 }
 
+export default BasicTypescriptTypes;
